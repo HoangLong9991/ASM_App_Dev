0e1454dd39357db31755c592ea883e0c8b370698
hoanglongle77 <longlnhgcd201639@fpt.edu.vn>
2022-08-05 19:23:43 +0700

    Hoang Long Le - Get all Books Data and Show Data in Index view

--- ASM_App_Dev/Controllers/BooksController.cs
+++ ASM_App_Dev/Controllers/BooksController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASM_App_Dev.Data;
+using ASM_App_Dev.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ASM_App_Dev.Controllers
 {
     public class BooksController : Controller
     {
+        // 1 - Declare ApplicationDbContext
+        private ApplicationDbContext _context;
+        public  BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // 2 - View Book Data
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Book> books = _context.Books.ToList();
+            return View(books);
         }
 
     }
