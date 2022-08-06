938f388e811367538dbf388c2da6fd21ed8d9953
HoangLong9991 <longnhgcd191143@fpt.edu.vn>
2022-08-06 15:22:06 +0700

    Add class order and orderdetail

---
+++ ASM_App_Dev/Data/Migrations/20220806082124_addOrderAndOrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASM_App_Dev.Data.Migrations
+{
+    public partial class addOrderAndOrderDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ASM_App_Dev/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using ASM_App_Dev.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASM_App_Dev.Models
+{
+	public class Order
+	{
+		[Key]
+		public int Id { get; set; }
+		public DateTime DateOrder { get; set; } = DateTime.Now;
+		public int PriceOrder { get; set; }
+		public OrderStatus StatusOrder { get; set; }
+		public List<OrderDetail> OrderDetails { get; set; }
+	}
+}
