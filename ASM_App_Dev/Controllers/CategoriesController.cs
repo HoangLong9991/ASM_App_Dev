280a5f689ae8bae5aa8239285e73f21577c7bafb
hoanglongle77 <longlnhgcd201639@fpt.edu.vn>
2022-08-23 21:15:24 +0700

    Hoang Long Le - Create Records Controller and Index View

--- ASM_App_Dev/Controllers/CategoriesController.cs
+++ ASM_App_Dev/Controllers/CategoriesController.cs
@@ -4,9 +4,13 @@ using ASM_App_Dev.Utils;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ASM_App_Dev.Controllers
 {
@@ -47,6 +51,7 @@ namespace ASM_App_Dev.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
 
     }
 }
---
+++ ASM_App_Dev/Controllers/RecordsController.cs
@@ -0,0 +1,24 @@
+﻿using ASM_App_Dev.Data;
+using ASM_App_Dev.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM_App_Dev.Controllers
+{
+    [Authorize(Roles = Role.STORE_OWNER)]
+    public class RecordsController : Controller
+    {
+        // 1 - Declare ApplicationDbContext
+        private ApplicationDbContext _context;
+        public RecordsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
