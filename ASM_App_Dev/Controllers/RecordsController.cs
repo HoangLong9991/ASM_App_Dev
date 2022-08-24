ecc01f777e4553fa92934061d38ac2c9ebe8d617
hoanglongle77 <longlnhgcd201639@fpt.edu.vn>
2022-08-24 13:21:44 +0700

    Hoang Long Le - Display Customer's Orders Data

--- ASM_App_Dev/Controllers/RecordsController.cs
+++ ASM_App_Dev/Controllers/RecordsController.cs
@@ -1,8 +1,14 @@
 ﻿using ASM_App_Dev.Data;
+using ASM_App_Dev.Models;
 using ASM_App_Dev.Utils;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+
+using System.Linq;
 
 namespace ASM_App_Dev.Controllers
 {
@@ -17,6 +23,14 @@ namespace ASM_App_Dev.Controllers
         }
 
         public IActionResult Index()
+        {
+            IEnumerable<Order> orders  = _context.Orders
+                .Include(t=>t.User)
+                .ToList();
+            return View(orders);
+        }
+
+        public IActionResult Details()
         {
             return View();
         }
