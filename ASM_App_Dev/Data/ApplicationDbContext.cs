d79f0497cadf9c729dc3a934c73d85570837812c
HoangLong9991 <longnhgcd191143@fpt.edu.vn>
2022-08-25 16:09:03 +0700

    Commit before pull

--- ASM_App_Dev/Data/ApplicationDbContext.cs
+++ ASM_App_Dev/Data/ApplicationDbContext.cs
@@ -16,23 +16,24 @@ namespace ASM_App_Dev.Data
 				: base(options)
 		{
 		}
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             this.SeedRoles(builder);
         }
-
         private void SeedRoles(ModelBuilder builder)
         {
             builder.Entity<IdentityRole>().HasData(
-            new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = Role.CUSTOMER, ConcurrencyStamp = "1", NormalizedName = Role.CUSTOMER },
-            new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = Role.ADMIN, ConcurrencyStamp = "2", NormalizedName = Role.ADMIN },
-            new IdentityRole() { Id = "0d3151ac-ebae-47d5-8bd1-ac783afafee2", Name = Role.STORE_OWNER, ConcurrencyStamp = "3", NormalizedName = Role.STORE_OWNER }
+            new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", 
+              Name = Role.CUSTOMER, ConcurrencyStamp = "1", NormalizedName = Role.CUSTOMER },
+            new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", 
+              Name = Role.ADMIN, ConcurrencyStamp = "2", NormalizedName = Role.ADMIN },
+            new IdentityRole() { Id = "0d3151ac-ebae-47d5-8bd1-ac783afafee2", 
+              Name = Role.STORE_OWNER, ConcurrencyStamp = "3", NormalizedName = Role.STORE_OWNER }
             );
         }
-public DbSet<Book> Books { get; set; }
-public DbSet<Category> Categories { set; get; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Category> Categories { set; get; }
     public DbSet<Order> Orders { set; get; }
     public DbSet<OrderDetail> OrderDetails { set; get; }
 
